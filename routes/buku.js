8c5ecc6d072e080ea39603d57c608fef7b255fb7
deckiherdiawans <d.herdiawan.s@gmail.com>
2019-07-21 11:53:33 +0700

    first commit

--- routes/buku.js
+++ routes/buku.js
@@ -4,18 +4,6 @@ const router = express.Router();
 
 const bukuController = require('../controllers/buku');
 
-router.get('/', bukuController.getAllBuku);
 
-router.get('/', bukuController.getBuku);
-
-router.post('/', bukuController.postBuku);
-
-// router.put('/', bukuController.putBuku);
-
-router.delete('/', bukuController.deleteBuku);
-
-// router.post('/search/:judul', bukuController.searchBook);
-
-// var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = router;
